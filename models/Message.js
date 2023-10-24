5e94abf002dd424591eff640140ec0142dd1ec47
= <79306624+kendricktty@users.noreply.github.com>
2023-10-24 21:52:12 +0800

    Log message model

---
+++ models/Message.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema({
+  message_id: {
+    type: String,
+  },
+  lock_id: {
+    type: String,
+  },
+  message: [{type: String}]
+});
+
+module.exports = mongoose.model("messages", MessageSchema);
